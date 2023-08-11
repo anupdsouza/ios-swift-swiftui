b6241ac131829cb99d727c2bccc29e64bf3c8f05
Anup D'Souza <103429618+anupdsouza@users.noreply.github.com>
2023-08-11 15:56:33 +0530

    Update checked continuations files

--- Concurrency/CheckedContinuations/CheckedContinuations/ContentView.swift
+++ Concurrency/CheckedContinuations/CheckedContinuations/ContentView.swift
@@ -7,15 +7,60 @@
 
 import SwiftUI
 
+struct DataLoader {
+    
+    func getData(url: URL) async throws -> Data {
+        return try await withCheckedThrowingContinuation { continuation in
+            URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+                if let data = data {
+                    continuation.resume(returning: data)
+                } else if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(throwing: URLError(.badServerResponse))
+                }
+            }.resume()
+        }
+    }
+}
+
+class ViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    private let dataLoader = DataLoader()
+    
+    func fetchImage() async {
+        guard let url = URL(string: "https://picsum.photos/300") else {
+            return
+        }
+        
+        do {
+            let data = try await dataLoader.getData(url: url)
+            if let img = UIImage(data: data) {
+                await MainActor.run {
+                    self.image = img
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
+
 struct ContentView: View {
+    @StateObject private var viewModel = ViewModel()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            }
             Text("Hello, world!")
         }
         .padding()
+        .task { [weak viewModel] in
+            await viewModel?.fetchImage()
+        }
     }
 }
 
