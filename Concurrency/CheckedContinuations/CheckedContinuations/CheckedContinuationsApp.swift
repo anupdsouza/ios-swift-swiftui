c58b791be7046e7af84d47f6921e85ae62fe05e2
Anup D'Souza <103429618+anupdsouza@users.noreply.github.com>
2023-08-11 14:22:25 +0530

    Add initial CheckedContinuations files

---
+++ Concurrency/CheckedContinuations/CheckedContinuations/CheckedContinuationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckedContinuationsApp.swift
+//  CheckedContinuations
+//
+//  Created by Anup D'Souza on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckedContinuationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
